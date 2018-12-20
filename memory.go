53fee21a375c2ee2c581bcd8754b3bcc4088e430
Nanite Factory <nanite@naver.com>
2018-12-20 21:47:19 +0900

    Unprotect() & WriteProcessMemory() which allows us to write something to memory

--- memory.go
+++ memory.go
@@ -1,26 +1,28 @@
 package memory
 
 import (
+	"errors"
 	"fmt"
 	"syscall"
 	"unsafe"
-
-	"github.com/nanitefactory/outputdbg"
 )
 
-//#include <Windows.h>
+//#include <windows.h>
+//#include <stdlib.h>
 import "C"
 
 // ----------------------------------------------------------------------------
 // Package variables; unexported.
 
 var (
-	hProcess            = _GetCurrentProcess()
-	hModExe             = _GetProcessModuleHandle()
-	hModKernel32        = kernel32.Handle()
-	kernel32            = syscall.NewLazyDLL("kernel32.dll") // Kernel32 APIs for accessing memory.
-	fnGetModuleHandleW  = kernel32.NewProc("GetModuleHandleW").Addr()
-	fnGetCurrentProcess = kernel32.NewProc("GetCurrentProcess").Addr()
+	hProcess             = _GetCurrentProcess()
+	hModExe              = _GetProcessModuleHandle()
+	hModKernel32         = kernel32.Handle()
+	kernel32             = syscall.NewLazyDLL("kernel32") // Kernel32 APIs for accessing memory.
+	fnGetModuleHandleW   = kernel32.NewProc("GetModuleHandleW").Addr()
+	fnGetCurrentProcess  = kernel32.NewProc("GetCurrentProcess").Addr()
+	fnVirtualProtectEx   = kernel32.NewProc("VirtualProtectEx").Addr()
+	fnWriteProcessMemory = kernel32.NewProc("WriteProcessMemory").Addr()
 )
 
 // ----------------------------------------------------------------------------
@@ -28,8 +30,8 @@ var (
 
 // GetProcessModuleHandle () returns the base address of .exe module.
 func _GetProcessModuleHandle() (hModule uintptr) {
-	hModule, _, _ = syscall.Syscall(fnGetModuleHandleW, 1, 0, 0, 0)              // (HANDLE)GetModuleHandle(NULL);
-	outputdbg.LogPrintln(fmt.Sprintf("GetProcessModuleHandle(): 0x%X", hModule)) //
+	hModule, _, _ = syscall.Syscall(fnGetModuleHandleW, 1, 0, 0, 0) // (HANDLE)GetModuleHandle(NULL);
+	// outputdbg.LogPrintln(fmt.Sprintf("GetProcessModuleHandle(): 0x%X", hModule)) //
 	return
 }
 
@@ -37,6 +39,7 @@ func _GetProcessModuleHandle() (hModule uintptr) {
 // This function is another simpler version of `func syscall.GetCurrentProcess() (pseudoHandle Handle, err error)`.
 func _GetCurrentProcess() (hProcess uintptr) {
 	hProcess, _, _ = syscall.Syscall(fnGetCurrentProcess, 0, 0, 0, 0)
+	// outputdbg.LogPrintln(fmt.Sprintf("GetCurrentProcess(): 0x%X", hProcess)) //
 	return
 }
 
@@ -45,22 +48,61 @@ func _GetCurrentProcess() (hProcess uintptr) {
 
 // GetHandleProcess () returns hProcess.
 func GetHandleProcess() uintptr {
-	outputdbg.LogPrintln(fmt.Sprintf("GetHandleProcess(): 0x%X", hProcess)) //
+	// outputdbg.LogPrintln(fmt.Sprintf("GetHandleProcess(): 0x%X", hProcess)) //
 	return hProcess
-}
-
-// GetHookPoint () in our process.
-func GetHookPoint(offset uintptr) (hookPoint uintptr) {
-	base := hModExe
-	hookPoint = base + offset
-	outputdbg.LogPrintln(fmt.Sprintf("GetHookPoint(): 0x%X = 0x%X + 0x%X", hookPoint, base, offset)) //
-	return
 }
 
 // GetBase () of our main module.
 func GetBase() uintptr {
 	// outputdbg.LogPrintln(fmt.Sprintf("GetBase(): 0x%X", hModExe)) //
 	return hModExe
+}
+
+// Unprotect a virtual memory region in order to get a permission to read/write there and get a way to protect it back after that.
+// nSize: Integer, the length in bytes.
+func Unprotect(whereFrom, nSize uintptr) (protectBack func() error, err error) {
+	var oldProtect C.DWORD
+	ret, _, err := syscall.Syscall6(
+		fnVirtualProtectEx,
+		5, hProcess, whereFrom, nSize, C.PAGE_EXECUTE_READWRITE, uintptr(unsafe.Pointer(&oldProtect)), 0,
+	)
+	// outputdbg.LogPrintln(ret, err) //
+	if ret != 1 {
+		return nil, errors.New(fmt.Sprint("Return code ", ret, ": ", err))
+	}
+	return func() error {
+		ret, _, err := syscall.Syscall6(
+			fnVirtualProtectEx,
+			5, hProcess, whereFrom, nSize, uintptr(oldProtect), uintptr(unsafe.Pointer(&oldProtect)), 0,
+		)
+		// outputdbg.LogPrintln(ret, err) //
+		if ret != 1 {
+			return errors.New(fmt.Sprint("Return code ", ret, ": ", err))
+		}
+		return nil
+	}, nil
+}
+
+// WriteProcessMemory is probably the safest way to write something to memory.
+//
+// Another way to write something unsafely to a memory region is as in,
+//
+// arr := (*[6]byte)(unsafe.Pointer(uintptr(0x004014D0))) // Where to write.
+// *arr = [6]byte{0x90, 0x90, 0x90, 0x90, 0x90, 0x90}     // AOB what to write.
+//
+func WriteProcessMemory(whereFrom uintptr, writeWhat []byte) error {
+	whatInHeap := C.CBytes(writeWhat)
+	defer C.free(whatInHeap)
+	fmt.Print() // We REQUIRE this for now because of that buggy C.CBytes(). If we don't do this, the syscall below will raise a fault.
+	ret, _, err := syscall.Syscall6(
+		fnWriteProcessMemory,
+		4, hProcess, whereFrom, uintptr(whatInHeap), uintptr(len(writeWhat)), 0, 0,
+	)
+	// outputdbg.LogPrintln(ret, err) //
+	if ret != 1 {
+		return errors.New(fmt.Sprint("Return code ", ret, ": ", err))
+	}
+	return nil
 }
 
 // GetPtrUnsafe () returns a ptr. Params must be in order.
